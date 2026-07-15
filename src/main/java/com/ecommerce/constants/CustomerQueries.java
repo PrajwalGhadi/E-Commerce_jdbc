bf5cd250a62d3fcee358043e2df83e4b6e8fd6c4
mrsahilansari0000 <mrsahilansari2003@gmail.com>
2026-07-15 19:12:42 +0530

    updated customer

--- src/main/java/com/ecommerce/constants/CustomerQueries.java
+++ src/main/java/com/ecommerce/constants/CustomerQueries.java
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
-
 package com.ecommerce.constants;
 
 public class CustomerQueries {
-	
-	private CustomerQueries() {
-		
-	}
-=======
-package com.ecommerce.constants;
-
-public class CustomerQueries {
->>>>>>> 0c28ce0c5a9f54c97c06002d7b6dbb8b6849e914
 
 	public String userSignUpQuery = "INSERT INTO User(user_id, username, email, password, created_at, role) values(?,?,?,?,?,?)";
 
